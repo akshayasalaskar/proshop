73560c7e06b14b728992b6b705c4dc6a058274bb
akshayasalaskar <akshayasalaskar03@gmail.com>
2024-03-20 13:02:10 +0530

    commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <p>Welcome to Proshop</p>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
